4fdc1d77e7c858c00101fdf76bbe4842eed5bd6d
AnastasiaCoder <mstitelnica1999@gmail.com>
2022-02-26 13:25:52 +0700

    добавлен пробел

--- src/test/java/com/amazon/test_data/Footer.java
+++ src/test/java/com/amazon/test_data/Footer.java
@@ -10,7 +10,7 @@ public class Footer {
     public static Stream<Arguments> footerColumns() {
         return Stream.of(
                 Arguments.of("Get to Know Us",
-                        List.of("Careers", "Blog", "About Amazon", "Investor Relations", "Amazon Devices","Amazon Science")),
+                        List.of("Careers", "Blog", "About Amazon", "Investor Relations", "Amazon Devices", "Amazon Science")),
                 Arguments.of("Amazon Payment Products",
                         List.of("Amazon Business Card", "Shop with Points", "Reload Your Balance", "Amazon Currency Converter"))
         );
